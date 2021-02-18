ef8551f1839d358579fd8eef566f2c16eb5de5e2
Natalia <nataliazh@wizardsdev.com>
2021-02-18 09:36:36 +0200

    added styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {Navbar} from './components/Navbar';
+import {Home} from './pages/Home'
+import {Article} from './pages/Article'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
-        <div className="container">
-            <h1>Sharetrade</h1>
-        </div>
+        <BrowserRouter>
+            <Navbar/>
+            <div className="container-fluid p-0 h-100">
+                <Switch>
+                    <Route path={'/'} exact component={Home}/>
+                    <Route path={'/article'} component={Article}/>
+                </Switch>
+            </div>
+        </BrowserRouter>
     );
 }
 
